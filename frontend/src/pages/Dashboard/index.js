1d62051c8466eed1c2750ff39989b33ec15183cc
aleodoni <aleodoni@gmail.com>
2019-10-07 18:41:58 -0300

    (frontend): Layout inicial do dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { MdAddCircleOutline } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Content, Header } from './styles';
 
 export default function Dashboard() {
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Content>
+        <Header>
+          <h1>Meus meetups</h1>
+          <button>
+            <MdAddCircleOutline size={24} />
+            Novo meetup
+          </button>
+        </Header>
+      </Content>
     </Container>
   );
 }
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -1,3 +1,57 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+
+  max-width: 1100px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 60px 0 0 0;
+  width: 100%;
+
+  h1 {
+    font-size: 36px;
+    color: #fff;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding-left: 25px;
+    padding-right: 25px;
+    height: 50px;
+    background: #d44059;
+    color: #fff;
+    transition: background 0.2s;
+    border: 0;
+    border-radius: 4px;
+    font-size: 20px;
+    font-weight: bold;
+
+    svg {
+      margin-right: 10px;
+    }
+
+    &:hover {
+      background: ${darken(0.1, '#d44059')};
+    }
+  }
+`;
